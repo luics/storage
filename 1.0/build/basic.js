df03f322d916e0a8eff2f0bdcb3e19f63709985b
鬼道 <xukai.xk@taobao.com>
2013-10-29 17:22:11 +0800

    ie7 「没有权限」问题

--- 1.0/build/basic.js
+++ 1.0/build/basic.js
@@ -6,7 +6,7 @@ gallery/storage/1.0/basic
 */
 /**
  * 由数据代理调用
- * 
+ *
  * @author luics (鬼道)
  */
 
@@ -132,20 +132,21 @@ KISSY.add('gallery/storage/1.0/basic', function(S, JSON) {
             }
             function withIEStorage(storeFunction) {
                 return function() {
-                    var args = Array.prototype.slice.call(arguments, 0)
-                    args.unshift(storage)
-                    // See http://msdn.microsoft.com/en-us/library/ms531081(v=VS.85).aspx
-                    // and http://msdn.microsoft.com/en-us/library/ms531424(v=VS.85).aspx
-                    storageOwner.appendChild(storage)
-                    storage.addBehavior('#default#userData')
-                    // FIXED why?
+                    // FIXED why? unshift load 均报过错
+
                     try {
+                        var args = Array.prototype.slice.call(arguments, 0)
+                        args.unshift(storage)
+                        // See http://msdn.microsoft.com/en-us/library/ms531081(v=VS.85).aspx
+                        // and http://msdn.microsoft.com/en-us/library/ms531424(v=VS.85).aspx
+                        storageOwner.appendChild(storage)
+                        storage.addBehavior('#default#userData')
                         storage.load(localStorageName)
+                        var result = storeFunction.apply(store, args)
+                        storageOwner.removeChild(storage)
+                        return result
                     } catch (e) {
                     }
-                    var result = storeFunction.apply(store, args)
-                    storageOwner.removeChild(storage)
-                    return result
                 }
             }
 
